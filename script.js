98418afad0a6b22acd0454240465e3f255dff529
Danielzx1 <daniel-oli55@hotmail.com>
2023-10-25 19:26:03 -0300

    adjust project for pessoal mode

--- script.js
+++ script.js
@@ -8,13 +8,9 @@ function toggleMode() {
   //substituir a imagem
   if (html.classList.contains("light")) {
     //se tiver light mode, adicionar a imagem light
-    img.setAttribute("src", "./assets/avatar-light.png")
-    img.setAttribute(
-      "alt",
-      "foto mayk brito sorrindo, de oculos escuro, jaqueta jeans e fundo azul"
-    )
+    img.setAttribute("src", "./assets/avatar 1.png")
   } else {
     // se tiver sem light mode, moantar a imagem normal
-    img.setAttribute("src", "./assets/avatar.png")
+    img.setAttribute("src", "./assets/avatar 3.png")
   }
 }
